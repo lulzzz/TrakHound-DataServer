2af1386344a0849876a2e28a41b10f2e963f9c6e
Patrick Ritchie <Patrick@TrakHound.com>
2016-12-17 22:33:03 -0500

    Updated solution

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TrakHound-Device-Server")]
+[assembly: AssemblyTitle("TrakHound Sniff")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrakHound-Device-Server")]
+[assembly: AssemblyProduct("Sniff")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
